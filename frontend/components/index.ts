b035676281e8d2f2abf138f867948d6c8a6531a7
RaphaelSilva09 <raphael.silva0907@gmail.com>
2026-01-14 21:17:58 -0300

    feat: exportar novos componentes no barrel file
    - Adicionar exports para BentoGridHome
    - Adicionar exports para BentoGridMiniLivros
    - Adicionar exports para Author
    - Facilitar imports centralizados de componentes

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -1,8 +1,9 @@
 // PP7+IAS Portal - Components Index
 // Regra de Negócio: O número 7
 
-export { default as BentoGrid } from "./BentoGrid";
+export { default as BentoGrid } from "./BentoGridHome";
 export { default as BentoGridNewsletter } from "./BentoGridNewsletter";
+export { default as BentoGridMiniLivros } from "./BentoGridMiniLivros";
 export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Header";
 export { default as HeroSection } from "./HeroSection";
