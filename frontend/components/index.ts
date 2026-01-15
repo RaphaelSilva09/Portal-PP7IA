aecb467344a697007e5fcf26ebea17a5f8f7fef5
RaphaelSilva09 <raphael.silva0907@gmail.com>
2026-01-15 17:39:01 -0300

    chore: exportar componente NewsletterCTA no índice
    - Adiciona export do NewsletterCTA
    - Mantém ordem alfabética das exportações
    - Facilita importação centralizada de componentes

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -1,7 +1,7 @@
 // PP7+IAS Portal - Components Index
 // Regra de Negócio: O número 7
 
-export { default as Author } from "./Author";
+export { default as BentoGridBiblioteca } from "./BentoGridBiblioteca";
 export { default as BentoGrid } from "./BentoGridHome";
 export { default as BentoGridMiniLivros } from "./BentoGridMiniLivros";
 export { default as BentoGridNewsletter } from "./BentoGridNewsletter";
@@ -9,3 +9,4 @@ export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Header";
 export { default as HeroSection } from "./HeroSection";
 export { default as HeroTitle } from "./HeroTitle";
+export { default as NewsletterCTA } from "./NewsletterCTA";
